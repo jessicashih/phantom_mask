0c86bc156e631d29847099e6748b058e412e1c04
jessicashih <10807003@pershing.com.tw>
2021-04-27 09:56:51 +0800

    [Feature] Project 基本設定

---
+++ phantommask/src/main/java/kdan/jessica/phantommask/PhantommaskApplication.java
@@ -0,0 +1,13 @@
+package kdan.jessica.phantommask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhantommaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhantommaskApplication.class, args);
+	}
+
+}
